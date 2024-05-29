4e0959a943fbf7ece9288b429de8f575eee86d78
phuongnguyen2022 <109055872+phuongnguyen2022@users.noreply.github.com>
2024-05-29 14:04:12 +0700

    tab bar function

---
+++ FoodDelivery/screens/Restaurant.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Restaurant = () => {
+  return (
+    <View>
+      <Text>Restaurant</Text>
+    </View>
+  );
+};
+export default Restaurant;
---
+++ FoodDelivery/screens/index.js
@@ -0,0 +1,10 @@
+import OrderDelivery from "./OrderDelivery";
+import Restaurant from "./Restaurant";
+import Home from "./Home";
+import React from "react";
+
+export{
+    Home,
+    Restaurant,
+    OrderDelivery
+}
