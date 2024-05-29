9e040055d7eb451d4251c3b106ac74f38abfd9fc
phuongnguyen2022 <109055872+phuongnguyen2022@users.noreply.github.com>
2024-05-29 18:43:54 +0700

    show food image
    done

---
+++ FoodDelivery/data/category.js
@@ -0,0 +1,26 @@
+import {icons} from '../constants';
+
+const categoryData = () => [
+  {
+    id: 1,
+    name: 'Rice',
+    icon: icons.rice_bowl,
+  },
+  {
+    id: 2,
+    name: 'Noodles',
+    icon: icons.noodle,
+  },
+  {
+    id: 3,
+    name: 'Hotdog',
+    icon: icons.hotdog,
+  },
+  {
+    id: 4,
+    name: 'Salad',
+    icon: icons.salad,
+  }
+];
+
+export default categoryData;
