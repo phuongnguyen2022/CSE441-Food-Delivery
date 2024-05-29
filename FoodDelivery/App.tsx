f3d87c2b79e2934880e0efa50fc4204dce88c5a7
phuongnguyen2022 <109055872+phuongnguyen2022@users.noreply.github.com>
2024-05-29 15:06:08 +0700

    header v1
    gg api???

--- FoodDelivery/App.tsx
+++ FoodDelivery/App.tsx
@@ -9,7 +9,10 @@ const Stack = createStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={{headerShown:false}
+      }
+      initialRouteName={'Home'}
+      >
         <Stack.Screen name="Home" component={Tabs} />
         <Stack.Screen name="Restaurant" component={Restaurant} />
         <Stack.Screen name="OrderDelivery" component={OrderDelivery} />
--- FoodDelivery/constants/maps.js
+++ FoodDelivery/constants/maps.js
@@ -1,3 +1,3 @@
-const GOOGLE_API_KEY = "YOUR GOOGLE API KEY"
+const GOOGLE_API_KEY = "https://www.google.com/maps/place/Eastern+International+University/@11.0530121,106.663594,17z/data=!3m1!4b1!4m6!3m5!1s0x3174d1d7df763eaf:0xf4323e44f2867057!8m2!3d11.0530121!4d106.6661689!16s%2Fg%2F1yw9kvsdy?entry=ttuhttps://maps.app.goo.gl/vzjJVxdwsy3zfDZq9"
 
 export default GOOGLE_API_KEY
